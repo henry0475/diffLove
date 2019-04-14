a5ded3e8add07f7678f1ee69f32792032bc5758d
liwenbin0320 <liwenbin@liwenbin.com>
2019-04-13 19:09:33 -0700

    1

--- services/dlMsgBoard/msgboard/list.go
+++ services/dlMsgBoard/msgboard/list.go
@@ -7,9 +7,9 @@ import (
 
 // GetMsgList will get a list of msg
 func GetMsgList(token string, startLine int64, offset int64) (msgListForProto []*dlMsgBoardProto.MsgInfoMsg, err error) {
-	msgList, err = libForMsgBoard.GetMsg(token, startLine, offset)
+	msgList, err := libForMsgBoard.GetMsg(token, startLine, offset)
 	for i, val := range msgList {
-		msgListForProto[i].ID = val.ID
+		msgListForProto[i].Id = val.ID
 		msgListForProto[i].Content = val.Content
 		msgListForProto[i].Publisher = val.Publisher
 		msgListForProto[i].Time = val.Time
